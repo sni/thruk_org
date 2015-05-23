7dbff762a9ba05167413c7a96cd43675ddc5bc1e
Sven Nierlein <sven@nierlein.de>
2015-05-23 18:47:19 +0200

    optimize images and js loading

---
+++ optimize_images.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+[ -f /usr/bin/optipng ] || { echo "optipng required!"; exit 1; }
+
+CMD="optipng -o7"
+
+if [ $# -gt 0 ]; then $CMD $*; exit; fi
+
+for file in $(find src/ -type f -name \*.png); do
+  echo $file;
+  $CMD $file 2>&1 | grep '^Output file size'
+done
